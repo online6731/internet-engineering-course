ca152bfdbd9c5268ba25cca5db35b8bedaacfd81
online6731 <mohammad.parsian@gmail.com>
2020-01-18 12:15:20 +0330

    exercise-2-3 login page updated

--- exercise-2-3/client/src/app/app-routing.module.ts
+++ exercise-2-3/client/src/app/app-routing.module.ts
@@ -2,7 +2,9 @@ import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [{ path: 'login', component: LoginComponent }];
+const routes: Routes = [
+  { path: '', component: LoginComponent },
+  { path: 'login', component: LoginComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
